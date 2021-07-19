7892e72882adde9bb9acdea90a42fd9961c6d394
restrictedCodex <restrictedcharudutta@gmail.com>
2021-07-19 15:34:09 +0530

    🔘 button link fixed

--- src/componenets/ButtonElements.js
+++ src/componenets/ButtonElements.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import {Link} from 'react-scroll'
 
-export const Button = styled(Link)`
+export const Button = styled.button`
   border-radius: 50px;
   background: ${({primary}) => (primary ?  '#04d9ff' : '#010606')};
   white-space: nowrap;
